879a6d374cd2a3085a4509803293930b48393a8a
scott <scotlandcloose@gmail.com>
2024-04-04 18:05:35 -0500

    progress on light-cover

--- CadQuery/light-cover/light-cover.py
+++ CadQuery/light-cover/light-cover.py
@@ -1,22 +1,31 @@
 import cadquery as cq
 
+if 'show_object' not in globals():
+    def show_object(*args, **kwargs):
+        pass
+
 def in_to_mm(inch):
     return inch * 25.4
 
 lamp_od = 159.75
 
-cover_thickness = 3 
+tolerance = -0.05
+
+cover_thickness = 3
 cover_rim_height = 9
+cover_id = lamp_od + tolerance
 cover_od = lamp_od + cover_thickness + cover_thickness * 2
+cover_or = cover_od / 2
 cover_height = cover_rim_height + cover_thickness
 
 
 
 cover = (
     cq.Workplane("XY")
-    .cylinder(height=cover_height, radius=cover_od/2)
+    .cylinder(height=cover_height, radius=cover_or)
     .faces(">Z")
-    .hole(diameter=lamp_od,depth=cover_rim_height)
+    .hole(diameter=cover_id ,depth=cover_rim_height)
+    .faces
     .faces("<Z")
     .edges()
     .fillet(2)
