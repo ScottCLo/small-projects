205e0a8e8f660a2c9845dc6f159366eda7e7016a
scott <scotlandcloose@gmail.com>
2024-04-09 17:59:31 -0500

    progress on light-cover2

---
+++ CadQuery/light-cover2/light-cover2.py
@@ -0,0 +1,31 @@
+import cadquery as cq
+
+if 'show_object' not in globals():
+    def show_object(*args, **kwargs):
+        pass
+
+def in_to_mm(inch):
+    return inch * 25.4
+
+cover_l = in_to_mm(9+7/8)
+cover_lip = 6
+cover_tab_w = 15
+
+light_d = in_to_mm(3)
+light_r = light_d/2
+
+profile = (
+    cq.Sketch("XY")
+    .circle(light_r)
+)
+
+cover = (
+    cq.Workplane("XY")
+    .box(cover_l,light_r,36.5)
+)
+
+assy = cq.Assembly()
+assy.add(cover, color=cq.Color(0.1,0.1,0.1), name="body")
+assy.save("out.step", "STEP", mode="fused")
+
+show_object(profile)
