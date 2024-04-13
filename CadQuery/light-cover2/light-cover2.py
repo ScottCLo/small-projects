ab3b9bda103f4dfb2acb4b5b903a0bf14c0ad970
scott <scotlandcloose@gmail.com>
2024-04-13 13:51:31 -0500

    progress on light-cover2

--- CadQuery/light-cover2/light-cover2.py
+++ CadQuery/light-cover2/light-cover2.py
@@ -12,19 +12,21 @@ cover_l = in_to_mm(9+7/8)
 cover_lip = 6
 cover_tab_w = 15
 
-light_d = in_to_mm(3)
-light_r = light_d/2
-light_face_w = 75
+light_d = 5#in_to_mm(3)
+light_r = 2.5
+light_face_w = light_d
+
+d = 2
 
 light_profile = (
         cq.Sketch()
-        .segment((0,-1), (0, 1), "s1")
-        .arc((0,-1), (1,0), (0,1), "a1")
-        .constrain("s1", "Length", 10)
+        .segment((0,-1),(0,1),"s1")
+        .constrain("s1", "Length", 6)
         .constrain("s1", "Fixed", None)
-        .constrain("a1", "Radius", 5 )
-        .constrain("s1","a1", "Coincident", None)
-        .constrain("a1","s1", "Coincident", None)
+        .arc((0,0),3,0,359, "a1")
+        #.constrain("a1","s1", "Coincident", None)
+        #.constrain("s1","a1", "Coincident", None)
+        #.constrain("a1", "Radius", light_r)
         .solve()
         )
 
